408bbc5ec62ea15e6a2e6cd655e478ed2b228d79
Melvinedl <medlund01@gmail.com>
2025-11-14 14:57:26 +0100

    -labb3

--- Labb3_Quiz/Labb3_Quiz/App.xaml.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Windows;
-
-namespace Labb3_Quiz;
-
-public partial class App : Application
-{
-}
--- Labb3_Quiz/Labb3_Quiz/Models/Question.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Labb3_Quiz.Models;
-
-public class Question
-{
-    public string Text { get; set; } = "";
-    public List<AnswerOption> Options { get; set; } = new();
-}
