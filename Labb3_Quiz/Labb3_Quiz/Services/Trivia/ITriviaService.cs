9884b3676dda6ed8d42692554b35331cab90aaf1
Melvin <medlund01@gmail.com>
2025-11-14 14:27:18 +0100

    Delete Labb3_Quiz directory

--- Labb3_Quiz/Labb3_Quiz/Services/Trivia/ITriviaService.cs
+++
@@ -1,9 +0,0 @@
-namespace Labb3_Quiz.Services.Trivia;
-
-public interface ITriviaService
-{
-    Task<List<OpenTdbCategory>> GetCategoriesAsync(CancellationToken ct = default);
-    Task<OpenTdbQuestionResponse> GetQuestionsAsync(
-        int amount, int? categoryId, string? difficulty, CancellationToken ct = default);
-}
-
